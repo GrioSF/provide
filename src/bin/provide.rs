3dcd0d0aed4a58a980ab7a20a358b967e9fb5a16
Adam Keating <1427265+akeating@users.noreply.github.com>
2019-03-31 09:41:13 -0700

    Update help handling

--- src/bin/provide.rs
+++ src/bin/provide.rs
@@ -1,11 +1,14 @@
 use provider::api::*;
-use clap::{App, Arg, SubCommand};
+use clap::{AppSettings, App, Arg, SubCommand, ArgMatches};
 use std::env;
 use rusoto_core::{Region};
+use rusoto_ssm::{Parameter};
 use std::str::FromStr;
+use std::error::Error;
 
-fn main() {
+fn main() -> Result<(), Box<Error>> {
     let matches = App::new("provide")
+        .setting(AppSettings::SubcommandRequiredElseHelp)
         .about("Provides environment variables from AWS Parameter Store")
         .subcommand(
             SubCommand::with_name("get")
@@ -27,39 +30,34 @@ fn main() {
                     .long("region")
                     .takes_value(true)
                     .value_name("REGION")
-                    .help("Specify region"))
+                    .help("Specify region (overrides env, profile)"))
         )
         .get_matches();
 
     match matches.subcommand() {
-        ("get", Some(get_matches)) => {
-            let path = get_matches.value_of("path").unwrap();
-            let region_name = get_matches.value_of("region");
-            let profile = get_matches.value_of("profile");
-            if profile.is_some() {
-                // The only way I've found to have rusoto honor given profile since ProfileProvider
-                // ignores it. Note this is only set for the current process.
-                env::set_var("AWS_PROFILE", profile.unwrap());
-            }
-            let region = match region_name {
-                Some(name) => match Region::from_str(name) {
-                    Ok(region) => region,
-                    Err(err) => return println!("{:?}", err)
-                },
-                None => Region::default() // will return a region defined in the env, profile, or default see method doc
-            };
-            do_get(String::from(path), region);
+        ("get", Some(matches)) => {
+            let (path, region) = process_get_matches(matches)?;
+            let parameters: Box<Vec<Parameter>> = get_parameters(path, region)?;
+            let map = to_hash_map(parameters);
+            print!("{}", as_env_format(map));
+            Ok(())
         },
-        _ => ()
+        _ => Ok(())
     }
 }
 
-fn do_get(path: String, region: Region) {
-    match get_parameters(path, region) {
-        Ok(parameters) => {
-            let map = to_hash_map(parameters);
-            print!("{}", as_env_format(map));
-        }
-        Err(err) => println!("{:?}", err),
+fn process_get_matches(matches: &ArgMatches) -> Result<(String, Region), Box<Error>> {
+    let path = matches.value_of("path").unwrap();
+    let region_name = matches.value_of("region");
+    let profile = matches.value_of("profile");
+    if profile.is_some() {
+        // The only way I've found to have rusoto honor given profile since ProfileProvider
+        // ignores it. Note this is only set for the current process.
+        env::set_var("AWS_PROFILE", profile.unwrap());
+    }
+    let region = match region_name {
+        Some(name) => Region::from_str(name)?,
+        None => Region::default() // will return a region defined in the env, profile, or default see method doc
     };
+    Ok((String::from(path), region))
 }
