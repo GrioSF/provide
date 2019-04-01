ac7bf876b7d2530b42cc12d272e6e992a3b797c2
Adam Keating <1427265+akeating@users.noreply.github.com>
2019-04-01 07:07:31 -0700

    Don't need these types anymore

--- src/types.rs
+++ src/types.rs
@@ -7,10 +7,3 @@ pub struct GetConfig {
     pub next_token: Option<String>, 
     pub acc: Box<Vec<Parameter>>
 }
-
-pub type App = String;
-pub type Env = String;
-pub type Key = String;
-pub type Val = String;
-pub type Triple = (App, Env, Key);
-pub type Quad = (App, Env, Key, Val);
