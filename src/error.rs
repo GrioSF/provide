c22f23ac758e2dec74c940a38038ee1ee9453a0e
Adam Keating <1427265+akeating@users.noreply.github.com>
2019-04-12 11:31:06 -0700

    Allow use of env vars for app and target

--- src/error.rs
+++ src/error.rs
@@ -3,6 +3,7 @@ use std::error::Error;
 use std::fmt;
 use std::io;
 use std::str;
+use std::env;
 use regex;
 use rusoto_core::region::ParseRegionError;
 use rusoto_ssm::{GetParametersByPathError};
@@ -10,6 +11,7 @@ use base64;
 
 #[derive(Debug, PartialEq)]
 pub enum ProvideError {
+    Error(String),
     BadRegex(regex::Error),
     BadFormat(String),
     InvalidPathError(String),
@@ -17,7 +19,8 @@ pub enum ProvideError {
     ParseRegionError(ParseRegionError),
     IOError(io::ErrorKind),
     Base64Error(base64::DecodeError),
-    UTF8Error(str::Utf8Error)
+    UTF8Error(str::Utf8Error),
+    EnvError(env::VarError)
 }
 
 impl Error for ProvideError {}
@@ -25,6 +28,7 @@ impl Error for ProvideError {}
 impl fmt::Display for ProvideError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
+            ProvideError::Error(message) => f.write_fmt(format_args!("Error: {}", message)),
             ProvideError::BadRegex(err) => f.write_fmt(format_args!("BadRegex: {:?}", err)),
             ProvideError::BadFormat(message) => f.write_fmt(format_args!("BadFormat: {}", message)),
             ProvideError::GetParametersByPathError(err) => f.write_fmt(format_args!("GetParametersByPathError: {}", err)),
@@ -33,6 +37,7 @@ impl fmt::Display for ProvideError {
             ProvideError::IOError(kind) => f.write_fmt(format_args!("IOError: {:?}", kind)),
             ProvideError::Base64Error(err) => f.write_fmt(format_args!("Base64Error: {}", err)),
             ProvideError::UTF8Error(err) => f.write_fmt(format_args!("UTF8Error: {}", err)),
+            ProvideError::EnvError(err) => f.write_fmt(format_args!("EnvError: {}", err)),
         }
     }
 }
@@ -64,5 +69,11 @@ impl From<base64::DecodeError> for ProvideError {
 impl From<str::Utf8Error> for ProvideError {
     fn from(err: str::Utf8Error) -> Self {
         ProvideError::UTF8Error(err)
+    }
+}
+
+impl From<env::VarError> for ProvideError {
+    fn from(err: env::VarError) -> Self {
+        ProvideError::EnvError(err)
     }
 }
