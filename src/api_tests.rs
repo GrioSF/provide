085edadaa4a48b18b78c76a2a6a0a0eaa8bc24a2
Adam Keating <1427265+akeating@users.noreply.github.com>
2022-05-28 10:13:59 -0700

    Update rust aws 0.12 (#8)
    * aws-sdk-rust v0.3.0
    
    * Updates resolve cargo audit issue
    
    * Update deprecated circleci xcode ver
    
    * Bump ver
    
    * 0.4.1 and new paginators api
    
    * Modest refactor
    
    * 0.5.2
    
    * 0.6.0
    
    * Updates
    
    Co-authored-by: Adam Keating <adamkeating@gmail.com>

---
+++ src/api_tests.rs
@@ -0,0 +1,98 @@
+use super::*;
+
+fn encode_pair(pair: Pair, use_base64: bool) -> String {
+  match pair {
+    Pair(key, val) => {
+      let encoded_val = if use_base64 {
+        base64::encode(&val)
+      } else {
+        val
+      };
+      format!("{key}={encoded_val}")
+    }
+  }
+}
+
+#[test]
+fn test_extract_key_from_path() {
+  assert_eq!(
+    extract_key_from_path("/app/env/DATABASE_URL").unwrap(),
+    "DATABASE_URL"
+  );
+  assert_eq!(extract_key_from_path("/app/env/foo").unwrap(), "foo");
+  assert_eq!(
+    extract_key_from_path("/app/foo").unwrap_err().to_string(),
+    Error::InvalidPathError(String::from("Invalid path /app/foo")).to_string()
+  );
+  assert_eq!(
+    extract_key_from_path("/app/foo/bar/car")
+      .unwrap_err()
+      .to_string(),
+    Error::InvalidPathError(String::from("Invalid path /app/foo/bar/car")).to_string()
+  );
+}
+
+#[test]
+fn test_as_env_format() {
+  let map: HashMap<String, String> = vec![
+    ("one".to_owned(), "bar".to_owned()),
+    ("two".to_owned(), "baz".to_owned()),
+    ("THREE".to_owned(), "clock".to_owned()),
+  ]
+  .into_iter()
+  .collect();
+  let env_format = as_env_format(map, true);
+  let mut result: Vec<&str> = env_format.trim().split("\n").collect();
+  result.sort();
+  assert_eq!(result, vec!["ONE=bar", "THREE=clock", "TWO=baz"]);
+}
+
+#[test]
+fn test_escape_for_bash() {
+  assert_eq!(escape_for_bash(r#"a$`"\'!)&"#), r#"a\$\`\"\\'\!\)&"#);
+}
+
+#[test]
+fn test_read_from_reader() {
+  let pair1 = encode_pair(Pair("foo".to_owned(), "bar".to_owned()), false);
+  let pair2 = encode_pair(Pair("baz".to_owned(), "qux".to_owned()), false);
+  let source = format!("{pair1}\n{pair2}\n").into_bytes();
+  let result = read_from_reader(BufReader::new(Cursor::new(source)), false).unwrap();
+  let expected: HashMap<String, String> = vec![
+    ("foo".to_owned(), "bar".to_owned()),
+    ("baz".to_owned(), "qux".to_owned()),
+  ]
+  .into_iter()
+  .collect();
+  assert_eq!(result, expected);
+}
+
+#[test]
+fn test_read_from_reader_as_base64() {
+  let pair1 = encode_pair(Pair("foo".to_owned(), "bar".to_owned()), true);
+  let pair2 = encode_pair(Pair("baz".to_owned(), "qux".to_owned()), true);
+  let source = format!("{pair1}\n{pair2}\n").into_bytes();
+  let result = read_from_reader(BufReader::new(Cursor::new(source)), true).unwrap();
+  let expected: HashMap<String, String> = vec![
+    ("foo".to_owned(), "bar".to_owned()),
+    ("baz".to_owned(), "qux".to_owned()),
+  ]
+  .into_iter()
+  .collect();
+  assert_eq!(result, expected);
+}
+
+#[test]
+fn test_read_from_reader_with_extra_lines() {
+  let pair1 = encode_pair(Pair("foo".to_owned(), "bar".to_owned()), true);
+  let pair2 = encode_pair(Pair("baz".to_owned(), "qux".to_owned()), true);
+  let source = format!("{pair1}\n\r\n\n{pair2}\n\n").into_bytes();
+  let result = read_from_reader(BufReader::new(Cursor::new(source)), true).unwrap();
+  let expected: HashMap<String, String> = vec![
+    ("foo".to_owned(), "bar".to_owned()),
+    ("baz".to_owned(), "qux".to_owned()),
+  ]
+  .into_iter()
+  .collect();
+  assert_eq!(result, expected);
+}
--- src/error.rs
+++ src/error.rs
@@ -1,38 +1,41 @@
+use aws_sdk_ssm::error::GetParametersByPathError;
+use aws_sdk_ssm::types::SdkError;
 use base64;
 use regex;
 use std::env;
 use std::str;
-use aws_sdk_ssm::SdkError;
-use aws_sdk_ssm::error::GetParametersByPathError;
 use thiserror::Error as ThisError;
 
 #[derive(ThisError, Debug)]
 pub enum Error {
-    #[error("ArgError: {0}")]
-    ArgError(#[from] clap::Error),
-    #[error("BadFormat: {0}")]
-    BadFormat(String),
-    #[error("BadRegex: {0}")]
-    BadRegex(#[from] regex::Error),
-    #[error("Base64Error: {0}")]
-    Base64Error(#[from] base64::DecodeError),
-    #[error("EnvError: {0}")]
-    EnvError(#[from] env::VarError),
-    #[error("Error: {0}")]
-    Error(String),
-    #[error("GetParametersByPathError: {0}")]
-    GetParametersByPathError(#[from] SdkError<GetParametersByPathError>),
-    #[error("InvalidPathError: {0}")]
-    InvalidPathError(String),
-    #[error("IOError: {0}")]
-    IOError(#[from] std::io::Error),
-    #[error("Utf8Error: {0}")]
-    StringUtf8Error(#[from] std::string::FromUtf8Error),
-    #[error("Utf8Error: {0}")]
-    StrUtf8Error(#[from] str::Utf8Error),
+  #[error("ArgError: {0}")]
+  ArgError(#[from] clap::Error),
+  #[error("BadFormat: {0}")]
+  BadFormat(String),
+  #[error("BadRegex: {0}")]
+  BadRegex(#[from] regex::Error),
+  #[error("Base64Error: {0}")]
+  Base64Error(#[from] base64::DecodeError),
+  #[error("EnvError: {0}")]
+  EnvError(#[from] env::VarError),
+  #[error("Error: {0}")]
+  Error(String),
+  #[error("GetParametersByPathError: {0}")]
+  GetParametersByPathError(#[from] SdkError<GetParametersByPathError>),
+  #[error("InvalidPathError: {0}")]
+  InvalidPathError(String),
+  #[error("IOError: {0}")]
+  IOError(#[from] std::io::Error),
+  #[error("Utf8Error: {0}")]
+  StringUtf8Error(#[from] std::string::FromUtf8Error),
+  #[error("Utf8Error: {0}")]
+  StrUtf8Error(#[from] str::Utf8Error),
 }
 
 #[test]
 fn test_error_display() {
-    assert_eq!("BadFormat: reasons", format!("{}", Error::BadFormat("reasons".into())))
+  assert_eq!(
+    "BadFormat: reasons",
+    format!("{}", Error::BadFormat("reasons".into()))
+  )
 }
